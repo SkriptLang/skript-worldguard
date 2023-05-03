5781493f0a82d46b0a8a735d58554c0535f9cae6
DereWah <daviloca03@gmail.com>
2023-05-03 23:47:05 +0200

    Code convention cleanup.

--- src/main/java/org/skriptlang/skriptworldguard/elements/expressions/ExprRegionFlag.java
+++ src/main/java/org/skriptlang/skriptworldguard/elements/expressions/ExprRegionFlag.java
@@ -1,131 +1,118 @@
 package org.skriptlang.skriptworldguard.elements.expressions;
 
-
-
+import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.expressions.base.PropertyExpression;
 import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
-
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.flags.*;
-
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-
-
 import org.bukkit.event.Event;
 import org.skriptlang.skriptworldguard.SkriptWorldGuard;
 import org.skriptlang.skriptworldguard.worldguard.WorldGuardRegion;
-
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class ExprRegionFlag extends PropertyExpression<WorldGuardRegion, String> {
+public class ExprRegionFlag extends PropertyExpression<WorldGuardRegion, Flag> {
 
     static {
-
-        register(ExprRegionFlag.class, String.class, "[worldguard] flag %string%", "worldguardregions");
-
+        Skript.registerExpression(ExprRegionFlag.class, Flag.class, ExpressionType.COMBINED,"[worldguard] flag[s] %strings%", "worldguardregions");
     }
 
-    private Expression<String> flag;
+    WorldGuard wg = WorldGuard.getInstance();
+    private Expression<String> exprFlag;
+    private Expression<WorldGuardRegion[]> exprRegions;
 
+    @Override
     @SuppressWarnings("unchecked")
-    @Override
     public boolean init(Expression<?>[] expression, int matchedPattern, Kleenean kleenean, ParseResult parseResult) {
-        flag = (Expression<String>) expression[0];
-        setExpr((Expression<? extends WorldGuardRegion>) expression[1]);
+        exprFlag = (Expression<String>) expression[0];
+        exprRegions = (Expression<WorldGuardRegion[]>) expression[1];
         return true;
-
     }
 
     @Override
-    protected String[] get(Event e, WorldGuardRegion[] region) {
-        Flag<?> fl = null;
-
-
-        WorldGuard wg = WorldGuard.getInstance();
-        fl = Flags.fuzzyMatchFlag(wg.getFlagRegistry(), flag.getSingle(e));
-
-        List<String> finalv = new ArrayList<>();
-
+    protected Flag[] get(Event event, WorldGuardRegion[] region) {
+        Flag<?> flag = Flags.fuzzyMatchFlag(wg.getFlagRegistry(), exprFlag.getSingle(event));
+        List<Flag> result = new ArrayList<>();
         for (WorldGuardRegion wrg : region){
             ProtectedRegion rg = wrg.getRegion();
             if (rg != null){
-                finalv.add(rg.getFlag(fl).toString());
+                result.add((Flag) rg.getFlag(flag));
             }
         }
-
-
-
-        return (String[]) finalv.toArray();
+        return (Flag[]) result.toArray();
     }
 
     @Override
-    public String toString(Event e, boolean debug) {
-
-        return "flag \"" + flag.getSingle(e) + "\" of region" ;
-    }
-
-    @Override
-    public Class<? extends String> getReturnType() {
-        return String.class;
-    }
-
-
-
-    @Override
-    public Class<?>[] acceptChange(final Changer.ChangeMode mode){
-        if (mode == Changer.ChangeMode.SET || mode == Changer.ChangeMode.DELETE) { return CollectionUtils.array(String.class);}
+    public Class<?>[] acceptChange(ChangeMode mode){
+        switch (mode){
+            case SET:
+            case DELETE:
+                return CollectionUtils.array(String.class);
+        }
         return null;
     }
 
-    public void change(Event e, Object[] delta, Changer.ChangeMode mode){
-
-        Flag<?> fl = null;
-
+    public void change(Event event, Object[] delta, Changer.ChangeMode mode){
+        Flag<?> flag;
+        if(exprFlag.getSingle(event) == null || exprRegions.getSingle(event) == null){
+            return;
+        }
         WorldGuard wg = WorldGuard.getInstance();
-        WorldGuardRegion[] regions = getExpr().getArray(e);
-
-        fl = Flags.fuzzyMatchFlag(wg.getFlagRegistry(), flag.getSingle(e));
+        WorldGuardRegion[] regions = getExpr().getArray(event);
+        flag = Flags.fuzzyMatchFlag(wg.getFlagRegistry(), exprFlag.getSingle(event));
         for (WorldGuardRegion region : regions){
             ProtectedRegion rg = region.getRegion();
-            if (rg != null && fl != null){
-                if (mode == Changer.ChangeMode.SET && delta != null){
-                    if (delta[0] instanceof Boolean) {
-                        if ((Boolean) delta[0]) {
-                            rg.setFlag((StateFlag) fl, StateFlag.State.ALLOW);
-                        } else {
-                            rg.setFlag((StateFlag) fl, StateFlag.State.DENY);
+            if (rg != null && flag != null){
+                switch(mode){
+                    case SET:
+                        if(delta != null){
+                            if (delta[0] instanceof Boolean) {
+                                if ((Boolean) delta[0]) {
+                                    rg.setFlag((StateFlag) flag, StateFlag.State.ALLOW);
+                                } else {
+                                    rg.setFlag((StateFlag) flag, StateFlag.State.DENY);
+                                }
+                            }
+                            else if (delta[0] instanceof String) {
+                                rg.setFlag((StringFlag) flag, (String) delta[0]);
+                            } else if (delta[0] instanceof Integer) {
+                                rg.setFlag((IntegerFlag) flag, (int) delta[0]);
+                            } else if (delta[0] instanceof Double) {
+                                rg.setFlag((DoubleFlag) flag, (double) delta[0]);
+                            } else {
+                                SkriptWorldGuard.getInstance().getLogger().warning("Region flag " + flag.getName() + " cannot be set to: " + delta[0]);
+                            }
                         }
-                    }
-                    else if (delta[0] instanceof String) {
-                        rg.setFlag((StringFlag) fl, (String) delta[0]);
-                    } else if (delta[0] instanceof Integer) {
-                        rg.setFlag((IntegerFlag) fl, (int) delta[0]);
-                    } else if (delta[0] instanceof Double) {
-                        rg.setFlag((DoubleFlag) fl, (double) delta[0]);
-                    } else {
-                        SkriptWorldGuard.getInstance().getLogger().warning("Region flag " + "\"" + fl.getName() + "\"" + " cannot be set to: " + delta[0]);
-                    }
-                }
-                else if(mode == Changer.ChangeMode.DELETE){
-                    rg.setFlag(fl, null);
-                }
-                else{
-                    SkriptWorldGuard.getInstance().getLogger().warning("A flag can only be set or cleared.");
+                    case DELETE:
+                        rg.setFlag(flag, null);
+                    default:
+                        Skript.error("A flag can only be set or cleared.");
                 }
             }else{
                 if (rg == null) {
-                    SkriptWorldGuard.getInstance().getLogger().warning("Could not find region " + "\"" + rg.getId()  +"\".");
+                    SkriptWorldGuard.getInstance().getLogger().warning("Could not find region " + rg.getId());
                 }
                 if (flag == null){
-                    SkriptWorldGuard.getInstance().getLogger().warning("Could not find flag " + "\"" + flag.getSingle(e) +"\".");
+                    SkriptWorldGuard.getInstance().getLogger().warning("Could not find flag " +  flag.getName());
                 }
             }
         }
+    }
+
+    @Override
+    public Class<? extends Flag> getReturnType() {
+        return Flag.class;
+    }
+
+    @Override
+    public String toString(Event event, boolean debug) {
+        return "flag " + exprFlag.toString(event, debug) + " of region " + exprRegions.toString(event, debug) ;
     }
 }
--- src/main/java/org/skriptlang/skriptworldguard/elements/expressions/ExprRegionPriority.java
+++ src/main/java/org/skriptlang/skriptworldguard/elements/expressions/ExprRegionPriority.java
@@ -1,18 +1,12 @@
 package org.skriptlang.skriptworldguard.elements.expressions;
 
-
-
 import ch.njol.skript.classes.Changer;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
-
 import ch.njol.util.Kleenean;
 import ch.njol.util.coll.CollectionUtils;
-
-
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-
 import org.bukkit.event.Event;
 import org.skriptlang.skriptworldguard.SkriptWorldGuard;
 import org.skriptlang.skriptworldguard.worldguard.WorldGuardRegion;
@@ -20,16 +14,13 @@ import org.skriptlang.skriptworldguard.worldguard.WorldGuardRegion;
 public class ExprRegionPriority extends SimplePropertyExpression<WorldGuardRegion, Number> {
 
     static {
-
         register(ExprRegionPriority.class, Number.class, "priority", "worldguardregions");
-
     }
-
 
     private Expression<WorldGuardRegion> region;
 
-    @SuppressWarnings("unchecked")
     @Override
+    @SuppressWarnings("unchecked")
     public boolean init(Expression<?>[] expression, int matchedPattern, Kleenean kleenean, ParseResult parseResult) {
         region = (Expression<WorldGuardRegion>) expression[0];
         return true;
@@ -37,58 +28,40 @@ public class ExprRegionPriority extends SimplePropertyExpression<WorldGuardRegio
 
     @Override
     public Number convert(WorldGuardRegion rg) {
-
-        Number value = rg.getRegion().getPriority();
-
-        return value;
+        return rg.getRegion().getPriority();
     }
-
-    @Override
-    protected String getPropertyName() {
-        return "priority";
-    }
-
-
-
-    @Override
-    public Class<? extends Number> getReturnType() {
-        return Number.class;
-    }
-
-
 
     @Override
     public Class<?>[] acceptChange(final Changer.ChangeMode mode){
-        if (mode == Changer.ChangeMode.SET ||
-            mode == Changer.ChangeMode.ADD ||
-            mode == Changer.ChangeMode.REMOVE) {
+        switch(mode){
+            case SET:
+            case ADD:
+            case REMOVE:
                 return CollectionUtils.array(Number.class);
-            }
-        else if (mode == Changer.ChangeMode.RESET ||
-                mode == Changer.ChangeMode.DELETE) {
+            case RESET:
+            case DELETE:
                 return CollectionUtils.array();
         }
         return null;
     }
 
-    public void change(Event e, Object[] delta, Changer.ChangeMode mode){
-
-
-        ProtectedRegion rg = region.getSingle(e).getRegion();
-
+    public void change(Event event, Object[] delta, Changer.ChangeMode mode){
+        ProtectedRegion rg = region.getSingle(event).getRegion();
         if (rg != null){
             if (delta != null) {
-                Integer d = ((Number) delta[0]).intValue();
-                if (mode == Changer.ChangeMode.SET) {
-                    rg.setPriority(((Number) delta[0]).intValue());
-                } else if (mode == Changer.ChangeMode.ADD) {
-                    rg.setPriority(rg.getPriority() + d);
-                } else if (mode == Changer.ChangeMode.REMOVE) {
-                    rg.setPriority(rg.getPriority() - d);
+                switch(mode){
+                    case SET:
+                        rg.setPriority(((Number) delta[0]).intValue());
+                    case ADD:
+                        rg.setPriority(rg.getPriority() + ((Number) delta[0]).intValue());
+                    case REMOVE:
+                        rg.setPriority(rg.getPriority() - ((Number) delta[0]).intValue());
                 }
             } else{
-                if (mode == Changer.ChangeMode.RESET || mode == Changer.ChangeMode.DELETE){
-                    rg.setPriority(0);
+                switch(mode){
+                    case RESET:
+                    case DELETE:
+                        rg.setPriority(0);
                 }
             }
         }else{
@@ -96,6 +69,13 @@ public class ExprRegionPriority extends SimplePropertyExpression<WorldGuardRegio
         }
     }
 
+    @Override
+    public Class<? extends Number> getReturnType() {
+        return Number.class;
+    }
 
-
+    @Override
+    protected String getPropertyName() {
+        return "priority";
+    }
 }
