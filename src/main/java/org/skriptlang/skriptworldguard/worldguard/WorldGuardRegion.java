1f7b9408c0e975c9f8b827369eb326d96cec472d
APickledWalrus <apickledwalrus@icloud.com>
2025-07-03 12:12:30 -0400

    Replace CondRegionContains with AnyContains

--- src/main/java/org/skriptlang/skriptworldguard/worldguard/WorldGuardRegion.java
+++ src/main/java/org/skriptlang/skriptworldguard/worldguard/WorldGuardRegion.java
@@ -1,14 +1,19 @@
 package org.skriptlang.skriptworldguard.worldguard;
 
+import ch.njol.skript.lang.util.common.AnyContains;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.skriptlang.skript.lang.converter.Converters;
 
 /**
  * A utility record for associating a {@link ProtectedRegion} with a {@link World}.
  * @param world The world {@code region} is within.
  * @param region WorldGuard region.
  */
-public record WorldGuardRegion(World world, ProtectedRegion region) {
+public record WorldGuardRegion(World world, ProtectedRegion region) implements AnyContains<Object> {
 
 	@Override
 	public boolean equals(Object other) {
@@ -21,6 +26,21 @@ public record WorldGuardRegion(World world, ProtectedRegion region) {
 	@Override
 	public String toString() {
 		return RegionUtils.toString(world, region.getId());
+	}
+
+	/*
+	 * AnyContains
+	 */
+
+	@Override
+	public boolean contains(Object object) {
+		Location location = Converters.convert(object, Location.class);
+		return location != null && region().contains(BukkitAdapter.asBlockVector(location));
+	}
+
+	@Override
+	public boolean isSafeToCheck(Object value) {
+		return value != null;
 	}
 
 }
