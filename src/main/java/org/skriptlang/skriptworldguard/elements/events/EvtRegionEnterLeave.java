8eb95f1561c4fde86e7eb3531762b93c7c96fc27
APickledWalrus <apickledwalrus@icloud.com>
2025-07-10 12:34:08 -0400

    Make world optional in "enter/leave" event

--- src/main/java/org/skriptlang/skriptworldguard/elements/events/EvtRegionEnterLeave.java
+++ src/main/java/org/skriptlang/skriptworldguard/elements/events/EvtRegionEnterLeave.java
@@ -1,6 +1,5 @@
 package org.skriptlang.skriptworldguard.elements.events;
 
-import ch.njol.skript.config.Node;
 import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
@@ -14,16 +13,14 @@ import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 import org.skriptlang.skript.bukkit.registration.BukkitRegistryKeys;
 import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos;
-import org.skriptlang.skript.log.runtime.SyntaxRuntimeErrorProducer;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 import org.skriptlang.skriptworldguard.worldguard.RegionEnterLeaveEvent;
-import org.skriptlang.skriptworldguard.worldguard.RegionUtils;
 import org.skriptlang.skriptworldguard.worldguard.WorldGuardRegion;
 
-public class EvtRegionEnterLeave extends SkriptEvent implements SyntaxRuntimeErrorProducer {
+public class EvtRegionEnterLeave extends SkriptEvent {
 
 	public static void register(SyntaxRegistry registry) {
-		String regionPattern = "[the] [worldguard] region[s] [with [the] (name[s]|id[s])|named] %*strings% (in|of) [[the] world] %*string%";
+		String regionPattern = "[the] [worldguard] region[s] [with [the] (name[s]|id[s])|named] %*strings% [(in|of) [[the] world] %-*string%]";
 		registry.register(BukkitRegistryKeys.EVENT, BukkitSyntaxInfos.Event.builder(EvtRegionEnterLeave.class, "Region Enter/Leave")
 				.supplier(EvtRegionEnterLeave::new)
 				.addEvent(RegionEnterLeaveEvent.class)
@@ -39,10 +36,9 @@ public class EvtRegionEnterLeave extends SkriptEvent implements SyntaxRuntimeErr
 				.addSince("1.0")
 				.build());
 		EventValues.registerEventValue(RegionEnterLeaveEvent.class, WorldGuardRegion.class, RegionEnterLeaveEvent::getRegion);
+		EventValues.registerEventValue(RegionEnterLeaveEvent.class, World.class, event -> event.getRegion().world());
 		EventValues.registerEventValue(RegionEnterLeaveEvent.class, MoveType.class, RegionEnterLeaveEvent::getMoveType);
 	}
-
-	private Node node;
 
 	private @Nullable Literal<String> regionIds;
 	private @Nullable Literal<String> world;
@@ -50,7 +46,6 @@ public class EvtRegionEnterLeave extends SkriptEvent implements SyntaxRuntimeErr
 
 	@Override
 	public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
-		node = getParser().getNode();
 		if (args.length != 0) {
 			//noinspection unchecked
 			regionIds = (Literal<String>) args[0];
@@ -69,27 +64,22 @@ public class EvtRegionEnterLeave extends SkriptEvent implements SyntaxRuntimeErr
 		if (regionIds == null) {
 			return true;
 		}
-		assert world != null;
 
-		// validate world
-		World world = Bukkit.getWorld(this.world.getSingle());
+		World expectedWorld;
 		if (world == null) {
-			error("The world '" + this.world.getSingle() + "' does not exist");
-			return false;
-		}
-
-		// map regions
-		String[] regionIds = this.regionIds.getAll();
-		WorldGuardRegion[] regions = new WorldGuardRegion[regionIds.length];
-		for (int i = 0; i < regionIds.length; i++) {
-			regions[i] = RegionUtils.getRegion(world, regionIds[i]);
-			if (regions[i] == null) {
-				error("The region '" + regionIds[i] + "' does not exist in the world '" + world.getName() + "'");
+			expectedWorld = null;
+		} else {
+			expectedWorld = Bukkit.getWorld(world.getSingle());
+			if (world == null) {
 				return false;
 			}
 		}
 
-		return SimpleExpression.check(regions, region -> region.equals(enterLeaveEvent.getRegion()), false, false);
+		String foundId = enterLeaveEvent.getRegion().name();
+		World foundWorld = enterLeaveEvent.getRegion().world();
+		return SimpleExpression.check(this.regionIds.getAll(),
+				expectedId -> expectedId.equalsIgnoreCase(foundId) && (expectedWorld == null || expectedWorld.equals(foundWorld)),
+				false, false);
 	}
 
 	@Override
@@ -104,21 +94,18 @@ public class EvtRegionEnterLeave extends SkriptEvent implements SyntaxRuntimeErr
 			builder.append("exit");
 		}
 		if (regionIds != null) {
-			assert world != null;
 			builder.append("of");
 			if (regionIds.isSingle()) {
 				builder.append("region");
 			} else {
 				builder.append("regions");
 			}
-			builder.append("named", regionIds, "in the world", world);
+			builder.append("named", regionIds);
+			if (world != null) {
+				builder.append("in the world", world);
+			}
 		}
 		return builder.toString();
-	}
-
-	@Override
-	public Node getNode() {
-		return node;
 	}
 
 }
